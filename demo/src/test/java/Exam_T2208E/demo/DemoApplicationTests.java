fe1e97ae2304e2cf12eca364ec6f3a76404628fd
minhkhanhh <102962225+minhkhanhh@users.noreply.github.com>
2024-07-08 20:01:27 +0700

    Add files via upload

---
+++ demo/src/test/java/Exam_T2208E/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package Exam_T2208E.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
