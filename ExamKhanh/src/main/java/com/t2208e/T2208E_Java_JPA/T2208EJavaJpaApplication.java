e4b1c9797c739052a2ebef7b1fba971497c842a7
minhkhanhh <102962225+minhkhanhh@users.noreply.github.com>
2024-07-08 20:19:54 +0700

    commit

---
+++ ExamKhanh/src/main/java/com/t2208e/T2208E_Java_JPA/T2208EJavaJpaApplication.java
@@ -0,0 +1,13 @@
+package com.t2208e.T2208E_Java_JPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T2208EJavaJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T2208EJavaJpaApplication.class, args);
+	}
+
+}
---
+++ ExamKhanh/src/main/java/com/t2208e/T2208E_Java_JPA/dto/EmployeeDto.java
@@ -0,0 +1,13 @@
+package com.t2208e.T2208E_Java_JPA.dto;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class EmployeeDto {
+    private Long id;
+    private String name;
+    private Double wage;
+}
